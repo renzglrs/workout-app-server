3b70c546f1d52f6930d02c19878b2a29277d9b83
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-04-24 08:59:21 +0800

    Deployment fix

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
+const port = 4000;
 
 // middleware
 app.use(express.json());
@@ -19,17 +20,13 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        // listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on port 4000');
-        })
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose.connect(process.env.MONGO_URI);
 
+mongoose.connection.once("open", () => console.log("Now connected to MongoDB  Atlas"));
 
-
-
+if (require.main === module) {
+  // "process.env.PORT || port" will use the environment variable if it is available OR will use port 4000 if none is defined
+  app.listen(process.env.PORT || port, () => {
+    console.log(`API is now online on port ${port}`);
+  });
+}
